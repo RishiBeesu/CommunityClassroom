d6b02dde2ef63bf4e5ec63e22d052ba946157e08
RishiBeesu <118705487+RishiBeesu@users.noreply.github.com>
2022-11-22 07:12:26 +0530

    Create Greet.java

---
+++ Java/Assignment/First-Java/Greet.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Greet {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = input.next();
+        System.out.println("Hello " + name +". How are you?");
+    }
+}
